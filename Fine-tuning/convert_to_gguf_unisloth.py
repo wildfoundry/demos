aacc7e37b809a31bfa11bc8922467a0b0ce438c1
GregorD1A1 <dudnikgrv@gmail.com>
2024-02-19 11:11:30 +0100

    working on Ollama modelfile

--- Fine-tuning/convert_to_gguf_unisloth.py
+++ Fine-tuning/convert_to_gguf_unisloth.py
@@ -2,4 +2,4 @@ from unsloth import FastLanguageModel
 
 
 model, tokenizer = FastLanguageModel.from_pretrained("marsrover_zephyr_lora", load_in_4bit=True)
-model.save_pretrained_gguf("gguf_model", tokenizer, quantization_method = "q4_k_m")
+model.save_pretrained_gguf("gguf_model", tokenizer, quantization_method = "q6_k")
