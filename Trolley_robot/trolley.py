00ffae8a41ce55826fdb582052c3ea536ad7cf99
GregorD1A1 <dudnikgrv@gmail.com>
2024-06-10 11:35:10 +0200

    working on prompt

--- Trolley_robot/trolley.py
+++ Trolley_robot/trolley.py
@@ -1,16 +1,33 @@
 from langchain_community.llms import Ollama
 from langchain.prompts import PromptTemplate
 from langchain.output_parsers import XMLOutputParser
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.manager import CallbackManager
+from zero_motor import Motor
+import time
 
 
-def move(llm_output):
-    print(llm_output["response"][1]["decision"])
+def move(direction):
+    print(f"Moving {direction}...")
+    motor = Motor("MOTOR1", 1)
+    if direction == "Left":
+        motor.forward(0.5)
+        time.sleep(5)
+        motor.stop()
+    elif direction == "Right":
+        motor.reverse(0.5)
+        time.sleep(5)
+        motor.stop()
 
+
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
 llm = Ollama(
-    model="dolphin-llama3",
+    model="openchat",
     temperature=0,
+    callbacks=callback_manager,
 )
+
 
 with open('trolley.prompt', 'r') as file:
     prompt_template = file.read()
@@ -18,12 +35,12 @@ prompt = PromptTemplate.from_template(prompt_template)
 
 
 if __name__ == '__main__':
-    right_object = "old woman"
-    left_object = "young dog"
+    right_object = "young horse"
+    left_object = "old bear"
 
+    print("Loading LLM...")
     chain = prompt | llm | XMLOutputParser()
-
     result = chain.invoke({"right_object": right_object, "left_object": left_object})
-    print(result)
+    direction = result["response"][1]["decision"]
 
-    move(result)
+    move(direction)
